e943b35bbe1ed5e1fd51b9cc3d5dcb48e09aa90e
StreamChuckieFinster <culevadaniel@gmail.com>
2026-03-29 13:04:50 +0100

    Implement background music with click to play
    Add background music that plays on first click

--- music.js
+++ music.js
@@ -1,1 +1,11 @@
+  <!-- Background music -->
+  <audio id="bg-music" loop>
+   <source src="path/to/coolauramoozig.ogg" type="audio/ogg">
+  </audio>
 
+  <script>
+    const music = document.getElementById("bg-music");
+
+    document.addEventListener("click", () => {
+      music.play();
+    }, { once: true });
